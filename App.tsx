df8737e176848677d3078580e33975c49c01dc9a
Christian Brandt <brandtc@gmail.com>
2025-07-02 16:44:39 -0400

    Added pinIcons + Style sheet

--- App.tsx
+++ App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-//import GoogleMapsLocator from './src/screens/googlemaps/GoogleMapsLocator';
-//import GoogleMapsLocatorPopup from './src/screens/googlemaps/GoogleMapsLocatorPopup';
-//import GoogleMapsLocator from './src/screens/googlemaps/GoogleMapsScreen';
-//import GoogleMapsScreen from './src/screens/googlemaps/GoogleMapsScreen';
-import FindPlaces from './src/screens/googlemaps/FindPlaces';
+import StoreFinder from './src/screens/googlemaps/StoreFinder';
 
 function App(): React.ReactElement {
-  return <FindPlaces />;
+  return <StoreFinder />;
 }
 
 export default App;
---
+++ src/screens/googlemaps/Styles.tsx
@@ -0,0 +1,59 @@
+import { StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+  calloutContainer: {
+    width: 200,
+    backgroundColor: 'white',
+    borderRadius: 12,
+    padding: 10,
+    elevation: 5, // Android shadow
+    shadowColor: '#000', // iOS shadow
+
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+  },
+
+  title: {
+    fontWeight: 'bold',
+    marginBottom: 4,
+    fontSize: 16,
+  },
+
+  offers: {
+    marginBottom: 8,
+    fontSize: 14,
+  },
+
+  button: {
+    backgroundColor: '#007AFF',
+    paddingVertical: 6,
+    paddingHorizontal: 10,
+    borderRadius: 8,
+  },
+
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: '600',
+  },
+
+  markerIcon: {
+    width: 45,
+    height: 45,
+    resizeMode: 'contain',
+  },
+  map: {
+    flex: 1,
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default styles;
