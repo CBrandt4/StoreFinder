0a4c6eb6cbcb9f4c671c530f5fcedf01ae46ed5a
Christian Brandt <brandtc@gmail.com>
2025-07-08 10:51:11 -0400

    added button to fetch stores (make api call)

--- src/screens/googlemaps/StoreFinder.tsx
+++ src/screens/googlemaps/StoreFinder.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, ActivityIndicator, Image } from 'react-native';
+import { View, Text, ActivityIndicator, Image, Button } from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import GetLocation from 'react-native-get-location';
 import { API_KEY } from '@env';
@@ -78,7 +78,8 @@ const StoreFinder = () => {
         const coords = { latitude: loc.latitude, longitude: loc.longitude };
         setLocation(coords);
         console.log('Current location:', coords);
-        fetchStores(coords);
+        //console.log('Fetching stores with coords:', coords);
+        // fetchStores(coords);
       })
       .catch(console.warn);
   }, [fetchStores]);
@@ -115,88 +116,100 @@ const StoreFinder = () => {
   }
 
   return (
-    <MapView
-      onRegionChangeComplete={setRegion}
-      provider={PROVIDER_GOOGLE}
-      style={styles.map}
-      region={{
-        latitude: location.latitude,
-        longitude: location.longitude,
-        latitudeDelta: 0.45,
-        longitudeDelta: 0.45,
-      }}
-      showsUserLocation
-    >
-      <Clusterer
-        data={markers}
-        region={region}
-        options={DEFAULT_OPTIONS}
-        mapDimensions={MAP_DIMENSIONS}
-        renderItem={item => {
-          if ('cluster_id' in item.properties) {
-            // Render a cluster marker
-            return (
-              //memoize cluster markers if flickering!!
-              <Marker
-                key={`cluster-${item.properties.cluster_id}`}
-                coordinate={{
-                  latitude: item.geometry.coordinates[1],
-                  longitude: item.geometry.coordinates[0],
-                }}
-              >
-                <View style={styles.ClusterMarker}>
-                  <Text>{item.properties.point_count}</Text>
-                </View>
-              </Marker>
-            );
-          } else {
-            // Render a single store marker
-            const place = item.properties.place;
-            let icon;
-            let key;
-            if (place.name && place.name.includes('Old Navy')) {
-              icon = require('../../../icons/oldnavyPin.png');
-              key = `oldnavy-${item.properties.id}`;
-            } else if (place.name && place.name.includes('Gap')) {
-              icon = require('../../../icons/gapPin.png');
-              key = `gap-${item.properties.id}`;
-            } else if (place.name && place.name.includes('Athleta')) {
-              icon = require('../../../icons/athletaPin.png');
-              key = `athleta-${item.properties.id}`;
-            } else if (place.name && place.name.includes('Banana Republic')) {
-              icon = require('../../../icons/brPin.png');
-              key = `br-${item.properties.id}`;
-            } else {
-              icon = require('../../../icons/defaultMarker.png');
-              key = `other-${item.properties.id}`;
-              console.warn(
-                `No specific icon for ${place.name}, using default.`,
-              );
-            }
-
-            return (
-              <Marker
-                key={key}
-                coordinate={{
-                  latitude: place.geometry.location.lat,
-                  longitude: place.geometry.location.lng,
-                }}
-              >
-                <Image source={icon} style={styles.markerIcon} />
-                <MarkerCallout
-                  loc={{
-                    title: place.name,
-                    offers: 'Earn 5% back in rewards!',
-                    lat: place.geometry.location.lat,
-                    lng: place.geometry.location.lng,
-                  }}
-                />
-              </Marker>
-            );
-          }
+    <View style={styles.mapContainer}>
+      <Button
+        title="Search this area"
+        onPress={() => {
+          console.log(
+            'Button pressed, fetching stores with this location:',
+            location,
+          );
+          fetchStores(location);
         }}
       />
-    </MapView>
+      <MapView
+        onRegionChangeComplete={setRegion}
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        region={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.45,
+          longitudeDelta: 0.45,
+        }}
+        showsUserLocation
+      >
+        <Clusterer
+          data={markers}
+          region={region}
+          options={DEFAULT_OPTIONS}
+          mapDimensions={MAP_DIMENSIONS}
+          renderItem={item => {
+            if ('cluster_id' in item.properties) {
+              // Render a cluster marker
+              return (
+                //memoize cluster markers if flickering!!
+                <Marker
+                  key={`cluster-${item.properties.cluster_id}`}
+                  coordinate={{
+                    latitude: item.geometry.coordinates[1],
+                    longitude: item.geometry.coordinates[0],
+                  }}
+                >
+                  <View style={styles.ClusterMarker}>
+                    <Text>{item.properties.point_count}</Text>
+                  </View>
+                </Marker>
+              );
+            } else {
+              // Render a single store marker
+              const place = item.properties.place;
+              let icon;
+              let key;
+              if (place.name && place.name.includes('Old Navy')) {
+                icon = require('../../../icons/oldnavyPin.png');
+                key = `oldnavy-${item.properties.id}`;
+              } else if (place.name && place.name.includes('Gap')) {
+                icon = require('../../../icons/gapPin.png');
+                key = `gap-${item.properties.id}`;
+              } else if (place.name && place.name.includes('Athleta')) {
+                icon = require('../../../icons/athletaPin.png');
+                key = `athleta-${item.properties.id}`;
+              } else if (place.name && place.name.includes('Banana Republic')) {
+                icon = require('../../../icons/brPin.png');
+                key = `br-${item.properties.id}`;
+              } else {
+                icon = require('../../../icons/defaultMarker.png');
+                key = `other-${item.properties.id}`;
+                console.warn(
+                  `No specific icon for ${place.name}, using default.`,
+                );
+              }
+
+              return (
+                <Marker
+                  key={key}
+                  coordinate={{
+                    latitude: place.geometry.location.lat,
+                    longitude: place.geometry.location.lng,
+                  }}
+                >
+                  <Image source={icon} style={styles.markerIcon} />
+                  <MarkerCallout
+                    loc={{
+                      title: place.name,
+                      offers: 'Earn 5% back in rewards!',
+                      lat: place.geometry.location.lat,
+                      lng: place.geometry.location.lng,
+                    }}
+                  />
+                </Marker>
+              );
+            }
+          }}
+        />
+      </MapView>
+    </View>
   );
 };
 
